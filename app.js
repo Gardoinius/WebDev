285ab1e5663549928d9fb18098727eb9ef520b11
Irvel <irvel.nduva@gmail.com>
2018-11-08 17:41:22 -0600

    Return json containing generated QR code

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ const express = require("express");
 const app = express();
 var path    = require("path");
 const port = 3000;
+var boleto = require('./boleto');
 
 app.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname+"/index.html"));
 });
-var boleto = require('./boleto');
 
-app.get('/codigoQR', function(req, res) { 
+app.get('/codigoQR', function(req, res) {
 	//res.set('Content-Type', 'image/png');
+	// res.send("prueba");
 	var codigoQR = boleto.generaCodigoQr('Codigo Prueba');
-	res.send(codigoQR);
+	res.json(codigoQR);
 });
 
 
--- boleto.js
+++ boleto.js
@@ -1,10 +1,17 @@
 var QRCode = require('qrcode')
 
-module.exports = { 
+module.exports = {
 	generaCodigoQr: function(folio) {
-		QRCode.toDataURL(folio, function (err, url) {
-  			console.log("Código QR generado")
-			return url;
-		})
+		let code_url = QRCode.toDataURL(folio, function (err, url) {
+		  		console.log("Código QR generado");
+		});
+
+		let json_object = JSON.stringify({
+	       status: 1,
+	       description: 'Success',
+	       qr_data: code_url
+		});
+
+		return json_object;
 	}
 };
