a8cae7897e78b75d39f9d3a65df2d1f1008513a3
Irvel <irvel.nduva@gmail.com>
2018-11-08 23:31:48 -0600

    Generate dummy PDF file from request

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const express = require("express");
+const pdf = require("express-pdf");
+const boleto = require("./boleto");
 const app = express();
-var path    = require("path");
+app.use(pdf);
+
 const port = 3000;
-var boleto = require('./boleto');
+
+const path = require("path");
 
 app.get("/", function(req, res) {
 	res.sendFile(path.join(__dirname+"/index.html"));
 });
 
-app.get('/codigoQR', function(req, res) {
-	//res.set('Content-Type', 'image/png');
+app.get("/codigoQR", function(req, res) {
+	//res.set("Content-Type", "image/png");
 	// res.send("prueba");
 	var codigoQR = boleto.generaCodigoQr('Codigo Prueba');
 	res.json(codigoQR);
+});
+
+// TODO: Dinamically generate route for dynamic pdf
+app.use("/generaBoletoPdf", function(req, res){
+    res.pdfFromHTML({
+        filename: "boleto.pdf",
+        // TODO: Inflate an HTML template file with the generated QR code
+        htmlContent: "<html><body>Boleto</body></html>",
+    });
 });
 
 
