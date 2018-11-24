c8e9daecbadf023d4fc1a5a9bb199fda5ee71aee
jorgegil96 <jorgegilcavazos@gmail.com>
2018-11-24 17:54:30 -0600

    Add new views and firebase db integration

--- app.js
+++ app.js
@@ -10,14 +10,25 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var boleto = require("./routes/boleto");
 
+const mustacheExpress = require('mustache-express');
+
+require('./firebase.config')
+const database = require('firebase').database()
+
 var app = express();
 app.use(pdf);
 
 const port = 3000;
 
 // view engine setup
+
+// app.engine('mst', mustache(VIEWS_PATH + '/partials', '.mst'));
+
+
+app.engine('mst', mustacheExpress())
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'mst')
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -27,28 +38,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 app.use('/users', users);
 app.use('/boleto', boleto);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+  
+//   res.send('error')
+//   // res.render('error');
+// });
 
 module.exports = app;
 
@@ -59,5 +71,60 @@ app.get("/codigoQR", function(req, res) {
 	res.json(codigoQR);
 });
 
+app.get("/eventos/:id", (req, res) => {
+	const ref = database.ref(`/events/${req.params.id}`)
+	ref.on('value', snapshot => {
+		const value = snapshot.val()
+		if (value) {
+			const { name, description, image, tickets } = value
+			const flattenedTickets = []
+
+			for (let ticketType of Object.keys(tickets)) {
+				flattenedTickets.push({
+					name: ticketType,
+					...tickets[ticketType],
+				})
+			}
+			return res.render('evento', { name, description, image, tickets: flattenedTickets })
+		}	else {
+			res.send('404')
+		}	
+	})  
+})
+
+app.get("/checkout", (req, res) => {	 
+		const { price, type } = req.query
+		console.log(req.query)
+		return res.render('checkout', { price, type })
+})
+
+
+app.get('/', (req, res) => {
+	const ref = database.ref(`/events`)
+	ref.on('value', snapshot => {
+
+		const value = snapshot.val()
+		if (value) {			
+
+			const events = []
+			
+			for (let name of Object.keys(value)) {
+				events.push({
+					id: name,
+					...value[name],
+				})
+			}
+
+			return res.render('index', { events })
+		}	else {
+			res.send('404')
+		}	
+	})  
+})
+
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.use('/js', express.static('js'));
+app.use('/css', express.static('css'));
+app.use('/img', express.static('img'));
+app.use('/images', express.static('images'));
---
+++ firebase.config.js
@@ -0,0 +1,12 @@
+const firebase = require('firebase')
+
+const config = {
+  apiKey: "AIzaSyBlh1S8dFi6GjX8j1NSvgYo1KtpUx_kkYM",
+  authDomain: "rodeo-a7187.firebaseapp.com",
+  databaseURL: "https://rodeo-a7187.firebaseio.com",
+  projectId: "rodeo-a7187",
+  storageBucket: "rodeo-a7187.appspot.com",
+  messagingSenderId: "664151621180",
+}
+
+firebase.initializeApp(config);
